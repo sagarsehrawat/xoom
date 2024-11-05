30ae779df4bc8a96843f812eb8729924c0554df7
Sagar Sehrawat <sagar02sehrawat@gmail.com>
2024-11-05 19:31:57 +0530

    name change

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'Xoom',
+  title: 'Connectly',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
